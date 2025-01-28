b446389b5c31b72f8e027f9868d7b950f7603a11
PatrickLeonardo <patricklf7@proton.me>
2025-01-28 15:41:41 -0300

    update about page

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
+// @ts-ignore:next-line
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
     plugins: [
         tailwindcss(),
     ],
+    build: {
+        rollupOptions: {
+            input: {
+                main: './index.html',
+                about: './sobre.html',
+            },
+        },
+    },
 })
