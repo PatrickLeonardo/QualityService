507cafc7c52144d2f1a110ca61f117f7ecf37599
PatrickLeonardo <patricklf7@proton.me>
2025-02-10 13:16:59 -0300

    updating index.js

--- api/index.js
+++ api/index.js
@@ -1,10 +1,15 @@
 import fs from "fs"
+import path from "path";
 import express from "express";
 import cors from 'cors';
 
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+const dataPath = path.join(__dirname, 'data.json');
+
 let data;
+
 try {
-    data = JSON.parse(fs.readFileSync('./data.json'));
+    data = JSON.parse(fs.readFileSync(dataPath));
 } catch (error) {
     console.error("Error reading data.json:", error);
     data = {};
