392f430456ad7e23baaa2d9ee4d8aa1b7a0a8237
PatrickLeonardo <patricklf7@proton.me>
2025-02-10 13:06:08 -0300

    updating package.json

--- api/index.js
+++ api/index.js
@@ -2,7 +2,14 @@ import fs from "fs"
 import express from "express";
 import cors from 'cors';
 
-const data = JSON.parse(fs.readFileSync('./data.json'));
+let data;
+try {
+    data = JSON.parse(fs.readFileSync('./data.json'));
+} catch (error) {
+    console.error("Error reading data.json:", error);
+    data = {};
+}
+
 const app = express();
 
 app.use(cors())
